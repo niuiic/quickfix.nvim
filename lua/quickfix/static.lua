02db03232d3a67adb9ac85fc117147e8888f918c
niuiic <niuiicde@gmail.com>
2023-04-29 09:41:37 +0800

    fix(config): clear hl_group

--- lua/quickfix/static.lua
+++ lua/quickfix/static.lua
@@ -1,27 +1,6 @@
 local config = {
 	create_dir = "mkdir -p",
-	hl_group = {
-		{
-			name = "QuickFixWarn",
-			match = {
-				"warn",
-				"Warn",
-				"warning",
-				"WARN",
-				"WARNING",
-			},
-			color = "#ff0000",
-		},
-		{
-			name = "QuickFixErr",
-			match = {
-				"error",
-				"Error",
-				"ERROR",
-			},
-			color = "#ffff00",
-		},
-	},
+	hl_group = {},
 }
 
 return {
